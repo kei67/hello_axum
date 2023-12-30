e9daf4259c571432abf8642f50394039c4d79846
kei67 <kei.02to.14@gmail.com>
2023-12-30 09:23:51 +0900

    Routing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,27 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-  // build our application with a single route
-  let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+  let app = Router::new()
+    .route("/", get(root))
+    .route("/foo", get(get_foo).post(post_foo))
+    .route("/foo/bar", get(foo_bar));
 
   // run our app with hyper, listening globally on port 3000
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  println!("listening on port 3000");
   axum::serve(listener, app).await.unwrap();
+}
+
+// which calls one of these handlers
+async fn root() -> String {
+  "hello".to_string()
+}
+async fn get_foo() -> String {
+  "foo".to_string()
+}
+async fn post_foo() -> String {
+  "post_foo".to_string()
+}
+async fn foo_bar() -> String {
+  "foo_bar".to_string()
 }
